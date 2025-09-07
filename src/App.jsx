397df0ad237d6328c60f7efc6ed0678a55052228
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-08 00:50:12 +0200

    feat(aggiungo funzioni e cancello il eventuale css e aggiungo bootstrap per dare completa funzionalita alla Navbar )

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import {Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
+import {Routes,Route} from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Products from './components/Products'
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,40 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
-      <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
-      <Link to="/about" style={{ margin: '0 10px' }}>Chi siamo</Link>
-      <Link to="/products" style={{ margin: '0 10px' }}>Prodotti</Link>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className='container'>
+        <NavLink className="navbar-brand" to="/">Store</NavLink>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink 
+               to="/" 
+                className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
+              >
+                  Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink 
+                to="/about" 
+                className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
+              >
+                Chi siamo
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink 
+                to="/products" 
+                className={({ isActive }) => "nav-link" + (isActive ? " active" : "")}
+              >
+                Prodotti
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   )
 }
