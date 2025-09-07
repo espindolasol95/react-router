8b3e3b587a88df1b94cdf97eb6754ad128c92d8a
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-07 22:48:24 +0200

    feat(file statico products)

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 const Products = () => {
   return (
     <div>
+        <h1> I nostri proddotti</h1>
+        <p>Qui troverai la lista dei prodotti</p>
       
     </div>
   )
