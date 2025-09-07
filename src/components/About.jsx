7a34290730e55fbef804b490d51c4a06a041223a
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-07 22:46:59 +0200

    feat(file statico About)

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 const About = () => {
   return (
     <div>
+        <h1>Chi siamo</h1>
+        <p>Azienda familiare italiana fondata a Trieste, nel 1933</p>
       
     </div>
   );
