1e4de2dfe786d2a51088df58dd1aded0ba49cfa3
espindolasol95 <espindolasol.ok@gmail.com>
2025-09-08 00:26:55 +0200

    feat(chiamata axios e inserzione parte funzionale in quella statica ora in schermo si pio visualizare perfettamente l'area prodotti)

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,14 +1,52 @@
 import React from 'react'
-import{Usestate, useEffect}
+import{useState, useEffect}from 'react'
+import axios from 'axios'
+function Products(){
+  const [products, setProducts]= useState([]) 
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-const Products = () => {
+
+  useEffect(()=>{
+     axios.get('https://fakestoreapi.com/products')
+     .then(response => {
+      setProducts(response.data) //salvo i prodotti nello stato
+      setLoading (false) 
+     })
+      .catch(err => {
+        setError(err.message)        // salvo eventuale errore
+        setLoading(false)
+
+  })
+
+ },[])
+
+ if (loading) return <p>Caricamento prodotti...</p>  //questo l'ho copiato non sono riuscita a ragionarlo prima
+ if (error) return <p>Errore: {error}</p>
+
+
+  //render della pag con i prodotti
   return (
-    <div>
-        <h1> I nostri proddotti</h1>
-        <p>Qui troverai la lista dei prodotti</p>
+    <div className="container mt-4">
+        <h1  className="mb-4"> I nostri proddotti</h1>
+        <div className="row">
+          {products.map(product =>(
+          <div className="col-md-3 mb-4" key={product.id}>
+            <div className="card h-100">
+              <img src={product.image} className="card-img-top p-3" alt={product.title} style={{height: '200px', objectFit: 'contain'}}/>
+              <div className="card-body d-flex flex-column">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text mt-auto">${product.price}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+        </div>
       
     </div>
   )
 }
+
+
 
 export default Products
